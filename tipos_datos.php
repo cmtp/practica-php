aa556f5ffd76ce55cbcaa2544eea1d8bb23893dc
cmtp <christian@koala-soft.com>
2015-03-01 00:40:51 -0400

    tipos de datos en php

---
+++ tipos_datos.php
@@ -0,0 +1,13 @@
+<?php
+ 	/*
+	 * Tipos de datos
+	 */
+ 	$num = 5;
+ 	$num2 = 24.5;
+ 	$estado = true;
+ 	$texto = "Cadena de texto";
+ 	echo $num."<br>";
+ 	echo $num2."<br>";
+ 	echo $estado."<br>";
+ 	echo $texto;
+?>
